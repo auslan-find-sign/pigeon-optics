03ee7018e9d459b91091bb75736538abd42d0635
Phoenix Fox <Bluebie@users.noreply.github.com>
2021-01-31 01:07:41 +1100

    progress on javascript lens model & vm setup (untested)

--- library/models/javascript-lens.js
+++ library/models/javascript-lens.js
@@ -4,7 +4,15 @@
  */
 const file = require('./cbor-file')
 const auth = require('./auth')
-const { VM, VMScript } = require('vm2')
+const ivm = require('isolated-vm')
+const codec = require('./codec')
+const fs = require('fs-extra')
+const defaults = require('../../package.json').defaults
+// Setup a VM for executing javascript lenses
+const Isolate = new ivm.Isolate({ memoryLimit: 64 })
+// Precompile the codec-lite.ivm init code
+const codecIvmCode = fs.readFileSync(require.resolve('./codec-lite.ivm.js')).toString()
+const codecScript = Isolate.compileScriptSync(codecIvmCode)
 
 module.exports = {
   /** returns a list of lenses the user has created
@@ -18,14 +26,14 @@ module.exports = {
   /** writes a lens to the user's data folder
    * @param {string} username - string username
    * @param {string} lens - string name of lens
-   * @param {string} lookupCode - lens code which returns an array of datasets/views to query
-   * @param {string} transformCode - lens code which returns an array of outputs
+   * @param {string} mapCode - lens code which returns an array of datasets/views to query
+   * @param {string} reduceCode - lens code which returns an array of outputs
    * @async
    */
-  async write (user, lens, lookupCode, transformCode) {
+  async write (user, lens, mapCode, reduceCode) {
     await file.write(`${auth.userFolder(user)}/javascript-lenses/${encodeURIComponent(lens)}/lens`, {
-      lookupCode,
-      transformCode,
+      mapCode,
+      reduceCode,
       updated: Date.now()
     })
   },
@@ -49,49 +57,85 @@ module.exports = {
     await file.delete(`${auth.userFolder(user)}/javascript-lenses/${encodeURIComponent(lens)}`)
   },
 
-  /** Return value of javascriptLens.load, containing lookup and transform functions
-   * @typedef {Object} JSLens
-   * @property {function} lookup - function(path, input) runs user supplied lookup code in a sandboxed VM, expects dataset entry or lens output as "input" argument
-   * @property {function} transform - function(path, input, lookups) expects input record from dataset or lens, and lookups should be an object containing stuff requested by the lookup function
+  /**
+   * async generator function which accepts an async iterable input that emits [recordPath, recordData]
+   * and outputs [recordID, recordData, dependencies]. dependencies will be an object with recordPath keys
+   * and buffer hash values. This object will always contain the input recordPath, and may contain extra
+   * mutable dependancies (/datasets/ and /viewports/ data paths) if the code used lookup() to fetch extra
+   * resources. Optional second argument logger can be set to a function which logs text out for debugging
+   * if set, logger must be a function which accepts logger('log'|'info'|'error'|'warn', ...args) and is
+   * otherwise similar to console.log/warn/error etc
+   * @typedef {function} LensMapFunction
    */
 
-  /** execute a javascript lens against an input data
+  /** load a javascript lens against an input data
    * @param {string} username - user who owns lens
    * @param {string} lens - name of javascript lens
-   * @param {object} globals - object with any extra globals to inject
-   * @param {number} timeout - how many milliseconds should this run for before giving up and throwing an error? default 10ms
-   * @returns {JSLens} - object with lookup and transform functions that can be run at any time
+   * @returns {LensMapFunction}
    * @async
    */
-  async load (user, lens, globals = {}, timeout = 10) {
-    // read in the script's code
-    const code = await module.exports.read(user, lens)
-    // create a VM for this script to run in
-    const vm = new VM({ timeout, sandbox: { }, eval: false, wasm: false, fixAsync: true })
-    // add lens metadata as a frozen global in the VM
-    vm.freeze({ owner: user, name: lens }, 'lens')
-    // add in any globals
-    vm.setGlobals(globals)
-    // create a VMScript compiled instance
-    const lookupScript = new VMScript(code.lookupCode)
-    const transformScript = new VMScript(code.transformCode)
-    // create executor function and return it
-    return {
-      lookup: (path, input) => {
-        // make input document available to the script
-        vm.setGlobal('input', input)
-        vm.setGlobal('inputID', inputID)
-        // return the results of running the script
-        return vm.run(lookupScript)
-      },
-      transform: (path, input, lookups) => {
-        // make input document available to the script
-        vm.setGlobal('input', input)
-        vm.setGlobal('path', path)
-        vm.setGlobal('lookups', lookups)
-        // return the results of running the script
-        return vm.run(transformScript)
+  async loadMap (user, lens) {
+    return async function * (iterator, logger = null) {
+      const config = await this.read(user, lens)
+      const mapScript = await Isolate.compileScript(`(function () {\n${config.mapCode}\n})()`, {
+        filename: `${defaults.url}/lenses/${user}:${lens}/functions/map.js`,
+        lineOffset: -1
+      })
+
+      for await (const [recordPath, recordData] of iterator) {
+        const context = await Isolate.createContext()
+        const outputs = []
+
+        // load embedded codec library
+        await codecScript.run(context)
+
+        // Adjust the jail to have a console.log/warn/error/info api, and to remove some non-deterministic features
+        if (!logger) logger = (type, ...args) => console.info(`Lens ${user}:${lens}/map console.${type}:`, ...args)
+        const emit = (recordID, recordData) => outputs.push([recordID, codec.cloneable.decode(recordData)])
+        await context.evalClosure(`
+        Math.random = function () { throw new Error('Math.random is non-deterministic and disallowed') }
+        function output(recordID, recordData) {
+          if (typeof recordID !== 'string' || recordID === '') throw new Error('recordID must be a non-empty string')
+          $1.applyIgnored(undefined, [recordID, codec.cloneable.encode(recordData)])
+        }
+        const console = Object.freeze({
+          log:   (...args) => $0.applyIgnored(undefined, ['log',   ...args], { arguments: { copy: true } }),
+          warn:  (...args) => $0.applyIgnored(undefined, ['warn',  ...args], { arguments: { copy: true } }),
+          error: (...args) => $0.applyIgnored(undefined, ['error', ...args], { arguments: { copy: true } }),
+          info:  (...args) => $0.applyIgnored(undefined, ['info',  ...args], { arguments: { copy: true } })
+        });`, [logger, emit], { arguments: { reference: true } })
+
+        // copy data in to context
+        await context.evalClosure(
+          'const recordPath = $0, recordData = codec.cloneable.decode($1);',
+          [recordPath, codec.cloneable.encode(recordData)],
+          { arguments: { copy: true } }
+        )
+
+        // run user script
+        mapScript.run(context, {
+          timeout: defaults.lensTimeout,
+          arguments: { reference: true }
+        })
+
+        // ask v8 to free this context's memory
+        context.release()
       }
     }
+  },
+
+  /**
+   * async function which accepts (recordID, data[]) as input
+   * and merges all the outputs of LensMapFunction that output the same recordID string, in to one value
+   * for use in a viewport, returns an arbitrary type
+   * @typedef {function} LensReduceFunction
+   */
+
+  /** load a javascript lens to use to reduce LensMapFunction outputs in to a final dataset
+   * @param {*} user - user who owns lens
+   * @param {*} lens - name of javascript lens
+   * @returns {function} LensReduceFunction
+   */
+  async loadReduce (user, lens) {
   }
 }
