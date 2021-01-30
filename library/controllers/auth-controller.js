3c28fbfec5e7419847aa3c6bcd4ba0a59a70a5ae
Phoenix Fox <Bluebie@users.noreply.github.com>
2021-01-31 01:04:12 +1100

    rewritten auth controller with Vibe views

--- library/controllers/auth-controller.js
+++ library/controllers/auth-controller.js
@@ -1,96 +1,42 @@
 const express = require('express')
-const router = express.Router()
-const ui = require('../ui')
+
 const auth = require('../models/auth')
-const serverTools = require('../server-tools')
-// const defaults = require('../../package.json').defaults
-const uri = require('encodeuricomponent-tag')
+const dataset = require('../models/dataset')
+const jsLens = require('../models/javascript-lens')
+const viewport = require('../models/viewport')
+
+const codec = require('../models/codec')
+
+const Vibe = require('../vibe/rich-builder')
+const loginView = require('../views/login')
+const profileView = require('../views/user-profile')
+
+const router = express.Router()
 
 // get a list of datasets owned by a specific user
 router.get('/auth/login', (req, res) => {
-  const memo = [
-    ui.paragraph({
-      contents: [
-        'Login with an existing account here, or ',
-        ui.link({ url: uri`register?return=${req.query.return || '/'}`, contents: 'register a new account' })
-      ]
-    })
-  ]
-
-  if (req.query.err) {
-    memo.push(ui.paragraph({ contents: [ui.glitch('Error: '), req.query.err] }))
-  }
-
-  serverTools.sendWebpage(req, res, {
-    title: 'Login to Datasets',
-    contents: [
-      ui.simpleForm({
-        title: 'Login to Datasets',
-        memo,
-        url: uri`login?return=${req.query.return || '/'}`,
-        method: 'POST',
-        fields: {
-          username: { type: 'text', value: req.query.username },
-          password: { type: 'password' }
-        },
-        buttonLabel: 'Login'
-      })
-    ]
-  })
+  Vibe.docStream('Login', loginView(req, { return: req.query.return || req.get('Referrer') || '/' })).pipe(res.type('html'))
 })
 
 router.post('/auth/login', async (req, res) => {
   try {
     req.session.auth = await auth.login(req.body.username, req.body.password)
-    res.redirect(req.query.return)
+    res.redirect(req.body.return)
   } catch (err) {
-    res.redirect(uri`/auth/login?username=${req.body.username}&err=${err.message}&return=${req.query.return}`)
+    Vibe.docStream('Login', loginView(req, req.body, err.message)).pipe(res.type('html'))
   }
-})
-
-// get a list of datasets owned by a specific user
-router.get('/auth/register', (req, res) => {
-  const memo = [
-    ui.paragraph({
-      contents: [
-        'Create a new account, or ',
-        ui.link({ url: uri`login?return=${req.query.return || '/'}`, contents: 'login to an existing account' })
-      ]
-    })
-  ]
-
-  if (req.query.err) {
-    memo.push(ui.paragraph({ contents: [ui.glitch('Error: '), req.query.err] }))
-  }
-
-  serverTools.sendWebpage(req, res, {
-    title: 'Register with Datasets',
-    contents: [
-      ui.simpleForm({
-        title: 'Register with Datasets',
-        memo,
-        url: `register?return=${req.query.return || '/'}`,
-        method: 'POST',
-        fields: {
-          username: { type: 'text', value: req.query.username },
-          password: { type: 'password' }
-        },
-        buttonLabel: 'Register'
-      })
-    ]
-  })
 })
 
 router.post('/auth/register', async (req, res) => {
   try {
     req.session.auth = await auth.register(req.body.username, req.body.password)
-    res.redirect(req.query.return)
+    res.redirect(req.body.return)
   } catch (err) {
     try {
       req.session.auth = await auth.login(req.body.username, req.body.password)
-      res.redirect(req.query.return)
+      res.redirect(req.body.return)
     } catch (err2) {
-      res.redirect(uri`/auth/register?username=${req.body.username}&err=${err.message}&return=${req.query.return}`)
+      Vibe.docStream('Login', loginView(req, req.body, err.message)).pipe(res.type('html'))
     }
   }
 })
@@ -98,6 +44,24 @@ router.post('/auth/register', async (req, res) => {
 router.get('/auth/logout', (req, res) => {
   delete req.session.auth
   res.redirect(req.query.return || req.get('Referrer') || '/')
+})
+
+router.get('/users/:user', async (req, res) => {
+  const profile = await auth.getProfile(req.params.user)
+  const datasets = await dataset.listDatasets(req.params.user)
+  const viewports = await viewport.listDatasets(req.params.user)
+  const jsLenses = await jsLens.list(req.param.user)
+
+  if (req.accepts('html')) {
+    Vibe.docStream(`${req.params.user}’s Profile`, profileView(req, profile, datasets, viewports, jsLenses)).pipe(res.type('html'))
+  } else {
+    codec.respond(req, res, {
+      auth: profile.auth,
+      datasets,
+      viewports,
+      lenses: jsLenses
+    })
+  }
 })
 
 module.exports = router
