8f681377f7539450a63c280c9d9790a51187d388
Phoenix Fox <Bluebie@users.noreply.github.com>
2021-02-05 20:07:34 +1100

    try to jail paths inside of cbor-file

--- library/models/cbor-file.js
+++ library/models/cbor-file.js
@@ -5,16 +5,26 @@ const os = require('os')
 const crypto = require('crypto')
 const defaults = require('../../package.json').defaults
 
+// normalise and check path for danger
+function fullPath (dataPath, suffix = '.cbor') {
+  if (`/${dataPath.join('/')}/`.includes('/../')) throw new Error('".." path segments aren\'t allowed for security')
+  const jail = path.resolve(defaults.data)
+  const segments = [defaults.data, dataPath].flat()
+  const result = `${path.resolve(...segments)}${suffix}`
+  if (!result.startsWith(jail)) throw new Error('path would escape data jail, nope!')
+  return result
+}
+
 /** Read a cbor encoded object from a path inside the data directory configured in package.json/defaults/data
  * If the data is unreadable or corrupt, attempts to read backup instead, printing an error in the process, if that's missing
  * or broken too, you can expect an error to throw, otherwise the older version will be returned with the error printed
- * @param {string} path - relative path inside data directory the data is located at
+ * @param {string|string[]} path - relative path inside data directory the data is located at
  * @returns {object}
  * @async
  */
 module.exports.read = async (dataPath) => {
   const tryRead = async (dataPath) => {
-    const buffer = await fs.readFile(path.join(defaults.data, `${dataPath}.cbor`))
+    const buffer = await fs.readFile(fullPath(dataPath))
     return cbor.decode(buffer)
   }
 
@@ -27,17 +37,17 @@ module.exports.read = async (dataPath) => {
 }
 
 /** Create or update a cbor data file, creating a .backup file of the previous version in the process
- * @param {string} path - relative path inside data directory the data is located at
+ * @param {string|string[]} path - relative path inside data directory the data is located at
  * @param {object} data - cbor encodable object to store
  * @async
  */
 module.exports.write = async (dataPath, data) => {
-  const fullPath = path.join(defaults.data, `${dataPath}.cbor`)
-  const backupPath = path.join(defaults.data, `${dataPath}.backup.cbor`)
+  const path1 = fullPath(dataPath)
+  const path2 = fullPath(dataPath, '.backup.cbor')
   const encoded = cbor.encode(data)
 
   // ensure directory exists
-  const folderPath = path.dirname(fullPath)
+  const folderPath = path.dirname(path1)
   await fs.ensureDir(folderPath)
 
   const rand = crypto.randomBytes(32).toString('hex')
@@ -45,39 +55,39 @@ module.exports.write = async (dataPath, data) => {
   await fs.writeFile(tempPath, encoded)
 
   // update backup with a copy of what was here previously if something old exists
-  if (await fs.pathExists(fullPath)) {
-    await fs.remove(backupPath)
-    await fs.move(fullPath, backupPath)
+  if (await fs.pathExists(path1)) {
+    await fs.remove(path2)
+    await fs.move(path1, path2)
   }
 
-  await fs.move(tempPath, fullPath)
+  await fs.move(tempPath, path1)
 }
 
 /** Remove a cbor data file
- * @param {string} path - relative path inside data directory the data is located at
+ * @param {string|string[]} path - relative path inside data directory the data is located at
  * @async
  */
 module.exports.delete = async (dataPath) => {
-  await fs.remove(path.join(defaults.data, `${dataPath}.cbor`))
-  await fs.remove(path.join(defaults.data, `${dataPath}.backup.cbor`))
+  await fs.remove(fullPath(dataPath))
+  await fs.remove(fullPath(dataPath, '.backup.cbor'))
 }
 
 /** Checks a given data path for an existing record, and returns true or false async
- * @param {string} path - relative path inside data directory
+ * @param {string|string[]} path - relative path inside data directory
  * @returns {boolean}
  * @async
  */
 module.exports.exists = async (dataPath) => {
   const results = await Promise.all([
-    fs.pathExists(path.join(defaults.data, `${dataPath}.cbor`)),
-    fs.pathExists(path.join(defaults.data, `${dataPath}.backup.cbor`))
+    fs.pathExists(fullPath(dataPath)),
+    fs.pathExists(fullPath(dataPath, '.backup.cbor'))
   ])
 
   return results.some(x => x === true)
 }
 
 /** List all the records in a data path
- * @param {string} path - relative path inside data directory to a folder containing multiple records
+ * @param {string|string[]} path - relative path inside data directory to a folder containing multiple records
  * @returns {string[]}
  * @async
  */
