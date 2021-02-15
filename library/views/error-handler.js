e3f32a96bc97d2e0571e000cd8cbf21a0049c98d
Phoenix Fox <Bluebie@users.noreply.github.com>
2021-02-16 09:41:09 +1100

    admins get stacktrace if logged in

--- library/views/error-handler.js
+++ library/views/error-handler.js
@@ -8,6 +8,6 @@ module.exports = (req, error) => {
   return layout(req, v => {
     v.heading(`Error processing request (${error.name} ${error.code}):`)
     v.p(error.message)
-    if (error.stack && process.env.NODE_ENV !== 'production') v.pre(error.stack)
+    if (error.stack && (process.env.NODE_ENV !== 'production' || (req.session && req.session.auth && req.session.auth.auth === 'admin'))) v.pre(error.stack)
   })
 }
