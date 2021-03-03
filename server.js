93e66b2aa1f4089f03b58182fea97e37ffdc2691
Phoenix Fox <Bluebie@users.noreply.github.com>
2021-03-04 09:30:32 +1100

    server will once a day try to prune a random attachment, just in case of bugs

--- server.js
+++ server.js
@@ -96,13 +96,13 @@ app.use((error, req, res, next) => {
   }
 })
 
-// if (settings.garbageCollectAttachmentsInterval) {
-//   const attachmentStore = require('./library/models/attachment-storage')
-//   function attachmentsGC () {
-//     attachmentStore.
-//   }
-//   setInterval(attachmentsGC, parseMs(settings.garbageCollectAttachmentsInterval).milliseconds)
-// }
+if (settings.garbageCollectAttachmentsInterval) {
+  const attachmentStore = require('./library/models/attachment-storage')
+  function attachmentsGC () {
+    attachmentStore.pruneRandom()
+  }
+  setInterval(attachmentsGC, parseMs(settings.garbageCollectAttachmentsInterval).milliseconds)
+}
 
 const port = process.env.PORT || 3000
 app.listen(port, '127.0.0.1', () => {
