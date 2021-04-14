9014b328b6750c19c1d72c79367790d98a8d2ebf
Phoenix Fox <Bluebie@users.noreply.github.com>
2021-04-14 14:48:46 +1000

    utility/http-error-handler improved handing of weird errors

--- library/utility/http-error-handler.js
+++ library/utility/http-error-handler.js
@@ -2,20 +2,24 @@ const createHttpError = require('http-errors')
 const codec = require('../models/codec')
 
 module.exports = ({ silent = false }) => {
-  return (error, req, res, next) => {
+  return async (error, req, res, next) => {
     if (res.headersSent) {
       return next(error) // delegate to default handler for situations where body is already returning
     }
 
-    if (createHttpError.isHttpError(error)) {
-      if (error.status) res.status(error.status)
-      if (error.headers) res.set(error.headers)
-    } else if (error.code === 'ENOENT') {
-      res.status(404) // something tried to read a file that doesn't exist
-    } else if (error.name === 'SyntaxError' || error.stack.includes('/borc/src/decoder.js')) {
-      res.status(400) // parse errors are likely to be clients sending malformed data
-    } else {
-      res.status(500)
+    // automatically guess some things when random errors fly in to the handler
+    if (!createHttpError.isHttpError(error)) {
+      if (!(error instanceof Error)) {
+        error = new Error(error)
+      }
+
+      error.status = error.statusCode = 500
+
+      if (error.code === 'ENOENT') {
+        error.status = error.statusCode = 404
+      } else if (error.name === 'SyntaxError' || error.stack.includes('/borc/src/decoder.js')) {
+        error.status = error.statusCode = 400
+      }
     }
 
     if (!silent && req.path !== '/favicon.ico') {
@@ -24,10 +28,13 @@ module.exports = ({ silent = false }) => {
       console.error(error.stack)
     }
 
+    if (error.status || error.statusCode) res.status(error.status || error.statusCode)
+    if (error.headers) res.set(error.headers)
+
     if (req.accepts('html')) {
-      res.sendVibe('error-handler', 'Request Error', error)
+      await res.sendVibe('error-handler', 'Request Error', error)
     } else {
-      codec.respond(req, res, {
+      await codec.respond(req, res, {
         error: error.message,
         stack: req.auth === 'admin' && error.stack
       })
