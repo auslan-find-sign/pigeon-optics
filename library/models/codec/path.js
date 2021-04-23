8b14cbaddec5c6a146d8c3603169d2763b1c34ca
Phoenix Fox <Bluebie@users.noreply.github.com>
2021-04-23 13:06:34 +1000

    models/codec/path updated to correctly do url percent encoding

--- library/models/codec/path.js
+++ library/models/codec/path.js
@@ -1,15 +1,21 @@
+const uri = require('encodeuricomponent-tag')
 const ptr = require('path-to-regexp')
+
 const datasetPath = '/:source(lenses|datasets|meta)/:user\\::name'
 const datasetMatch = ptr.match(datasetPath)
-const datasetCompile = ptr.compile(datasetPath)
+// const datasetCompile = ptr.compile(datasetPath)
 const recordPath = `${datasetPath}/records/:recordID`
 const recordMatch = ptr.match(recordPath)
-const recordCompile = ptr.compile(recordPath)
+// const recordCompile = ptr.compile(recordPath)
 
 Object.assign(exports, {
   decode (string) {
     const out = datasetMatch(string) || recordMatch(string)
-    return out ? { ...out.params } : out
+    if (out) {
+      return Object.fromEntries(Object.entries(out.params).map(([prop, value]) => [prop, decodeURIComponent(value)]))
+    } else {
+      return out
+    }
   },
 
   encode (source, user, name, recordID = undefined) {
@@ -17,10 +23,12 @@ Object.assign(exports, {
       return this.encode(source.source, source.user, source.name, source.recordID)
     }
 
+    if (!['lenses', 'datasets', 'meta'].includes(source)) throw new Error('Unknown source')
+
     if (typeof recordID === 'string') {
-      return recordCompile({ source, user, name, recordID })
+      return uri`/${source}/${user}:${name}/records/${recordID}` // recordCompile({ source, user, name, recordID })
     } else {
-      return datasetCompile({ source, user, name })
+      return uri`/${source}/${user}:${name}` // datasetCompile({ source, user, name })
     }
   }
 })
