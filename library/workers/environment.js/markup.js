fa705cd1a308dfebd02111ef82de2b619ef515f8
Phoenix Fox <Bluebie@users.noreply.github.com>
2021-05-03 11:38:00 +1000

    better jsdoc in workers/environment.js/markup.js

--- library/workers/environment.js/markup.js
+++ library/workers/environment.js/markup.js
@@ -5,9 +5,9 @@ const utils = require('pigeonmark-utils')
 const querySelector = treeSelector.createQuerySelector(treeAdapter)
 /**
  * Query a JsonML xml document using a css selector string, get an array of results
- * @param {JsonMLElement} root - root element of the document, or an object with a JsonML property containing such
- * @param {*} selector - basic CSS selector to query the document with
- * @returns {JsonMLElement[]}
+ * @param {utils.PMRootNode} root - root element of the document, or an object with a JsonML property containing such
+ * @param {string} selector - basic CSS selector to query the document with
+ * @returns {utils.PMNode[]}
  */
 exports.select = function select (root, selector) {
   // get treeAdapter to learn the child -> parent mapping, necessary for css selecting
@@ -21,14 +21,14 @@ exports.isPigeonMark = utils.isPigeonMark
 
 /**
  * Given a JsonML element, or a string, render it to a HTML string, suitably escaped and structured
- * @param {string|Array} element
+ * @param {utils.PMRootNode} element
  * @returns {string}
  */
 exports.toHTML = require('pigeonmark-html/library/encode')
 
 /**
  * Given a JsonML element, or a string, render it to a HTML string, suitably escaped and structured
- * @param {string|Array} element
+ * @param {utils.PMRootNode} element
  * @returns {string}
  */
 exports.toXML = require('pigeonmark-xml/library/encode')
