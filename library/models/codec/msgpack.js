c1290b412b24dd738ead616f7766018d1c3b8de8
Phoenix Fox <Bluebie@users.noreply.github.com>
2021-05-06 15:28:54 +1000

    msgpack codec gains explicit Set encoding

--- library/models/codec/msgpack.js
+++ library/models/codec/msgpack.js
@@ -1,4 +1,14 @@
-const msgpack = require('msgpack5')
-module.exports = msgpack()
-module.exports.handles = ['application/msgpack', 'application/x-msgpack']
-module.exports.extensions = ['msgpack']
+const msgpack = require('msgpack5')()
+msgpack.handles = ['application/msgpack', 'application/x-msgpack']
+msgpack.extensions = ['msgpack']
+
+// hash a name in to an id number between 0 and 128, to use as an extension point type number
+function nameToExtID (name) {
+  return 1 + Array.prototype.reduce.call(name, (a, b) => a + parseInt(b, 36), 0) % 127
+}
+
+const setTypeNumber = nameToExtID('set') // 72
+// Set's are encoded to an extension, which contains a messagepack encoded array of the set's elements
+msgpack.register(setTypeNumber, Set, set => msgpack.encode([...set]), buffer => new Set(msgpack.decode(buffer)))
+
+module.exports = msgpack
