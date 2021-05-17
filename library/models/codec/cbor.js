2693057afa6a80461a8abdb77578176320634d84
Phoenix Fox <Bluebie@users.noreply.github.com>
2021-05-17 23:56:18 +1000

    models/codec/cbor is more codesense compatible

--- library/models/codec/cbor.js
+++ library/models/codec/cbor.js
@@ -1,34 +1,40 @@
 const cbor = require('cbor')
 
-Object.assign(exports, {
-  handles: ['application/cbor', 'application/x-cbor'],
-  extensions: ['cbor'],
-  decoderOpts: {},
-  encoderOpts: { highWaterMark: 25000000 },
+exports.handles = ['application/cbor', 'application/x-cbor']
+exports.extensions = ['cbor']
+exports.decoderOpts = {}
+exports.encoderOpts = { highWaterMark: 25000000 }
 
-  /**
-   * Decodes first item in buffer using CBOR, unpacking any attachments included in the cbor too
-   * @param {Buffer} buffer - buffer containing cbor which optionally uses tag 27 object representation to include Attachment and AttachmentReference objects
-   * @returns {any} - returns decoded object
-   */
-  decode (inputBuffer) {
-    return cbor.decodeFirstSync(inputBuffer, this.decoderOpts)
-  },
+/**
+ * Encodes a CBOR encodable object, with support for this application's Attachment and AttachmentReference objects
+ * @param {any} data - arbitrary object to encode
+ * @returns {Buffer} - cbor buffer
+ */
+exports.encode = function (data) {
+  return cbor.encodeOne(data, this.encoderOpts)
+}
 
-  decoder () {
-    return new cbor.Decoder(this.decoderOpts)
-  },
+/**
+ * Decodes first item in buffer using CBOR, unpacking any attachments included in the cbor too
+ * @param {Buffer} buffer - buffer containing cbor which optionally uses tag 27 object representation to include Attachment and AttachmentReference objects
+ * @returns {any} - returns decoded object
+ */
+exports.decode = function (inputBuffer) {
+  return cbor.decodeFirstSync(inputBuffer, this.decoderOpts)
+}
 
-  encoder () {
-    return new cbor.Encoder(this.encoderOpts)
-  },
+/**
+ * Create a transform stream which transforms a buffer stream in to an object stream using cbor streaming decoding
+ * @returns {import('stream').Transform}
+ */
+exports.decoder = function () {
+  return new cbor.Decoder(this.decoderOpts)
+}
 
-  /**
-   * Encodes a CBOR encodable object, with support for this application's Attachment and AttachmentReference objects
-   * @param {any} data - arbitrary object to encode
-   * @returns {Buffer} - cbor buffer
-   */
-  encode (data) {
-    return cbor.encodeOne(data, this.encoderOpts)
-  }
-})
+/**
+ * Create a transform stream which transforms a readable object stream to a binary encoded stream
+ * @returns {import('stream').Transform}
+ */
+exports.encoder = function () {
+  return new cbor.Encoder(this.encoderOpts)
+}
