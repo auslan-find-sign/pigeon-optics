18446f2a797c58010abbdf63c88442eadd4266ab
Phoenix Fox <Bluebie@users.noreply.github.com>
2021-05-24 00:38:46 +1000

    models/codec/object-hash accepts "undefined" as a valid root value

--- library/models/codec/object-hash.js
+++ library/models/codec/object-hash.js
@@ -1,9 +1,13 @@
 const hash = require('object-hash')
+
+const symUndef = Symbol('undefined proxy')
+
 /**
  * uses object-hash npm package to hash a complex object, like those stored in datasets or viewports
  * @param {any} object - input object to hash, maybe containing attachments
  * @returns {Buffer} - sha256 hash (32 bytes) in a nodejs Buffer
  */
 module.exports = function objectHash (object) {
+  if (object === undefined) object = symUndef
   return hash(object, { algorithm: 'sha256', encoding: 'buffer' })
 }
